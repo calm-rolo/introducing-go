c03040ac6c6adf844690cbb94310ab1b41d103b5
Carolina Rolo <organa.skyw@gmail.com>
2021-12-27 22:55:15 -0300

    Adding more files Chapter 1 t0 4 about variable types

--- hello.go
+++ hello.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+// My First Go Program, printing a message "Hello, World"
 func main(){
-  fmt.Println("Ola Mundo")
+  fmt.Println (len("Ola Mundo"))
+  fmt.Println("Hello Mundo"[1])
+  fmt.Println("Hello" + "Mundo")
 }
